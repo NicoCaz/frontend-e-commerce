e0a3158636c6b0e8a7dda63540f4f581d066c54f
NicoCaz <nicolascazorlamartinez@gmail.com>
2022-12-14 16:26:10 -0300

    probando cositas

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Square extends React.Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    constructor(props){
+        super(props);
+        this.state={
+            value:null,
+        };
+    }
+
+
+
+    render() {
+      return (
+        <button className="square" 
+        
+        onClick={() => this.setState({value: 'X'})}>
+          {this.state.value}
+        </button>
+      );   
+    }
+  }
+  
+  class Board extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            squares: Array(9).fill(null),
+        };
+    }
+  
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]} />;
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  // ========================================
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Game />);
+  
